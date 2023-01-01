94a0a887524f403264459d70904d578eb75e773f
Ian White <ian.white@hey.com>
2023-01-01 12:09:27 -0500

    Add vertical scrolling to the sidebar.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import '../styles/globals.css';
-import '@radix-ui/colors/blackA.css';
 import '@radix-ui/colors/whiteA.css';
-import '@radix-ui/colors/violet.css';
 
 import type { AppProps } from "next/app";
 
